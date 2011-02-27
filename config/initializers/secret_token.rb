a308e49c2df07cb0031b95a83c7ef661a5de0664
elhu <boisard.v@gmail.com>
2011-02-27 18:11:24 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openauthenticator::Application.config.secret_token = 'c29c70d09aaf841164a51fc7a7c1404b94c517676f77957610e827025c5233f7d1a2213080d7ad858241c3960465c96ac92fa8e4c2abcf48e3463971725daa95'
