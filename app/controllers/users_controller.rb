f6bc61513eda65b6e5273d063075f988130d3bb2
elhu <boisard.v@gmail.com>
2011-06-16 17:27:47 +0100

    added debug info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,8 @@ class UsersController < ApplicationController
     @users = User.all
 
     respond_to do |format|
-      format.html # index.html.erb
       format.xml { render :xml => @users }
+      format.json { render :json => @users }
     end
   end
 
